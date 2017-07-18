c06e23a985d6969b1b7b7d31da72ffeaa5c50508
Charles <charles.belzile@hotmail.com>
2017-07-18 14:42:46 -0400

    Added realisation

---
+++ src/src/Components/Realisation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1> R&eacute;alisations </h1>
+    <hr style="width:50%;">
+    <div class="jumbotron text-left">
+      <div class="container text-left">
+        <div class="row text-left">
+          <div class="col-xs-12 col-sm-3 col-md-2 text-left">
+            <img src="../assets/geektactics.png" class="img-responsive img-circle" alt="Reflet Communication">
+          </div>
+          <div class="col-xs-12 col-sm-9 col-md-10 text-left">
+            <h5> Conception d'une application web pour des &eacute;tudiants de l'universit&eacute; de Montr&eacute;al </h5>
+            <h6> <a href="http://www.geektactics.ca/">Geektactics.ca</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="jumbotron text-left">
+      <div class="container text-left">
+        <div class="row text-left">
+          <div class="col-xs-12 col-sm-3 col-md-2 text-left">
+            <img src="../assets/canada.png" class="img-responsive" alt="Gouvernement du Canada">
+          </div>
+          <div class="col-xs-12 col-sm-9 col-md-10 text-left">
+            <h5> Participation &agrave; l'&eacute;dition 2016 de Geekweek, &eacute;v&eacute;nement dans le domaine de la s&eacute;curit&eacute; informatique </h5>
+            <h6> <a href="https://g33kw33k.ca/">Geekweek</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'accueil',
+  data () {
+    return {
+      showLang: false
+    }
+  }
+}
+</script>
